e2f76ec6389c4e377cf484ac0801d74a453614ad
unknown <lakhi>
2022-02-09 17:12:48 +1300

    collection

--- src/collection/MapExample.java
+++ src/collection/MapExample.java
@@ -19,38 +19,39 @@ public class MapExample {
 		
 		System.out.println(map1);
 		
-		map1.put(2, "john");
+		/*map1.put(2, "john");
 		System.out.println(map1); //Update the data;
 		
 		map1.remove(3);// remove the data;
-		System.out.println(map1);
+		System.out.println(map1); */
 		
 	
 	//================================================
-	
+	/*
 		LinkedHashMap<Integer,String> map2 =new LinkedHashMap<Integer,String>();
 	
 		map2.put(1, "Lakhvinder Singh");
 		map2.put(2, "Yashdeep  Singh");
 		map2.put(3, "Harleen Kaur");
-		System.out.println(map2);
+		System.out.println(map2);*/
 		
 	//======================================================
 	
-		TreeMap<Integer,String> map3 =new TreeMap<Integer,String>();
+		/*TreeMap<Integer,String> map3 =new TreeMap<Integer,String>();
 	
 	
 		map3.put(1, "Lakhvinder Singh");
 		map3.put(2, "Yashdeep  Singh");
-		map3.put(3, "Harleen Kaur");
+		map3.put(3, "Harleen Kaur"); */
 	
-	//for( Map.Entry<Integer,String> entry1 :map1.entrySet()) 
-	//{
+	for( Map.Entry<Integer,String> entry1 :map1.entrySet()) 
+	{
 	
 	//System.out.println(entry1.getKey());
 	//System.out.println(entry1.getValue());
-	//}
-	
+	System.out.println(entry1.getValue());
+	System.out.println(entry1.getKey());
+	}
 	}
 }
 
